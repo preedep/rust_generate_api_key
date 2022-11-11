166f36b27378c01efd32dfae069eae4bd4990491
Preedee <>
2022-11-11 20:18:41 +0700

    - update code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use rand::Rng;
+use base64::encode;
 
+#[derive(Debug)]
+struct APIKey {
+    api_key: String,
+    secret_key: String
+}
+fn generate_api_key() -> APIKey {
+
+    let random_bytes_api_key: Vec<u8> = (0..32).map(|_| { rand::random::<u8>() }).collect();
+
+    let random_bytes_secret_key : Vec<u8> = (0..128).map(|_| { rand::random::<u8>() }).collect();
+
+    let api_key = APIKey{
+        api_key:encode(random_bytes_api_key),
+        secret_key:encode(random_bytes_secret_key)
+    };
+
+    return api_key;
+}
 fn main() {
-    let random_bytes: Vec<u8> = (0..1024).map(|_| { rand::random::<u8>() }).collect();
-    println!("{:?}", random_bytes);
+    let api_key = generate_api_key();
+    println!("{:?}", api_key);
 }
